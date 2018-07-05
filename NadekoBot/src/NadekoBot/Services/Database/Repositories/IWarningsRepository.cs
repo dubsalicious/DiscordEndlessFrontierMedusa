029d166f5988ab92c4f4f1b044121ef67f93fd13
newbstier <34542420+newbstier@users.noreply.github.com>
2018-07-05 23:12:00 +0300

    Update IWarningsRepository.cs

--- NadekoBot/src/NadekoBot/Services/Database/Repositories/IWarningsRepository.cs
+++ NadekoBot/src/NadekoBot/Services/Database/Repositories/IWarningsRepository.cs
@@ -6,6 +6,7 @@ namespace NadekoBot.Services.Database.Repositories
     public interface IWarningsRepository : IRepository<Warning>
     {
         Warning[] For(ulong guildId, ulong userId);
+        bool Forgive(ulong guildId, ulong userId, string mod, int index);
         Task ForgiveAll(ulong guildId, ulong userId, string mod);
         Warning[] GetForGuild(ulong id);
     }
