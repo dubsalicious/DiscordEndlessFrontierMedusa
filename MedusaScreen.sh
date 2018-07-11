108faec2593caad951aaae962619ca3512f97aef
PravEF <pravwlum@gmail.com>
2018-07-11 22:31:37 +0100

    Removing MedusaScreen.sh

--- MedusaScreen.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cd /home/discord/medusa
-rm screenlog.0
-screen -LS medusa -dm bash -c 'cd /home/discord/medusa/NadekoBot/src/NadekoBot; dotnet restore; dotnet run --configuration Release'
